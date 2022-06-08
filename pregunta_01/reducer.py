77be96d2a3d1c9a172867c21c8371d3e2a34ee6f
carboledad <101777132+carboledad@users.noreply.github.com>
2022-06-08 16:50:49 -0500

    Prueba2_Reduce
    Prueba2_Reduce

--- pregunta_01/reducer.py
+++ pregunta_01/reducer.py
@@ -1,6 +1,11 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+
+def make_counts(acc, nxt):
+    acc[nxt] = acc.get(nxt, 0) + 1
+    return acc
+
 from functools import reduce
 reduce(
     make_counts,
