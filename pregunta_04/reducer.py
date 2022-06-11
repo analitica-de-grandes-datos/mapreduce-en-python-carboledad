4c37ec7527909f5a91434c20046824c55a926429
Cesar Augusto Arboleda Diosa <101777132+carboledad@users.noreply.github.com>
2022-06-11 09:15:16 -0500

    Reducer_Pregunta4
    Reducer_Pregunta4

--- pregunta_04/reducer.py
+++ pregunta_04/reducer.py
@@ -1,3 +1,11 @@
 #
-# >>> Escriba el codigo del reducer a partir de este punto <<<
+# >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+    for line in sys.stdin:
+        # se separan las columnas separadas
+        lista = line.split("\t")
+        # se genera la salida del dato llamando la columna objetivo
+        sys.stdout.write("{},{}\n".format(lista[0].strip(), lista[1]))
