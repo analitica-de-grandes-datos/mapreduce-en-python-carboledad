64c85ed7561469e03138cfeadd36a50747a53a60
Cesar Augusto Arboleda Diosa <101777132+carboledad@users.noreply.github.com>
2022-06-11 17:04:47 -0500

    commit

--- pregunta_04/mapper.py
+++ pregunta_04/mapper.py
@@ -1,11 +1,8 @@
-#
-# >>> Escriba el codigo del mapper a partir de este punto <<<
-#
 import sys
 
 if __name__ == "__main__":
     for line in sys.stdin:
         # se separan las columnas separadas
-        lista = line.split("\t")
+        lista = line.split(" ")
         # se genera la salida del dato llamando la columna objetivo
-        sys.stdout.write("{},{}\n".format(lista[0], lista[1]))
+        sys.stdout.write("{},1\n".format(lista[0]))
